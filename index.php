88f9f6ff0cd4c85336b94424b0e2222b35c425a6
Nik0-9 <filannino.nicola9@gmail.com>
2024-04-30 17:00:24 +0200

    snack 2

--- index.php
+++ index.php
@@ -1,79 +1,95 @@
 <?php
 $partite = [
     [
-    "squadra_casa" => "Celtics",
-    "squadra_ospite" => "Lakers",
-    "punti_casa" => 102,
-    "punti_ospite" => 95
+        "squadra_casa" => "Celtics",
+        "squadra_ospite" => "Lakers",
+        "punti_casa" => 102,
+        "punti_ospite" => 95
     ],
     [
-    "squadra_casa" => "Bulls",
-    "squadra_ospite" => "Warriors",
-    "punti_casa" => 110,
-    "punti_ospite" => 105
+        "squadra_casa" => "Bulls",
+        "squadra_ospite" => "Warriors",
+        "punti_casa" => 110,
+        "punti_ospite" => 105
     ],
     [
-    "squadra_casa" => "Knicks",
-    "squadra_ospite" => "Rockets",
-    "punti_casa" => 95,
-    "punti_ospite" => 92
-],
-    ["squadra_casa" => "Heat",
-    "squadra_ospite" => "Spurs",
-    "punti_casa" => 115,
-    "punti_ospite" => 108
-],
+        "squadra_casa" => "Knicks",
+        "squadra_ospite" => "Rockets",
+        "punti_casa" => 95,
+        "punti_ospite" => 92
+    ],
     [
-    "squadra_casa" => "Thunder",
-    "squadra_ospite" => "Clippers",
-    "punti_casa" => 98,
-    "punti_ospite" => 103
+        "squadra_casa" => "Heat",
+        "squadra_ospite" => "Spurs",
+        "punti_casa" => 115,
+        "punti_ospite" => 108
+    ],
+    [
+        "squadra_casa" => "Thunder",
+        "squadra_ospite" => "Clippers",
+        "punti_casa" => 98,
+        "punti_ospite" => 103
     ]
 ];
-function control(){
-    $name = '';
-    $email = '';
-    $age = '';
-    $name = $_GET['name'];
-    $email = $_GET['email'];
-    $age = $_GET['age'];
-    var_dump($name);
+function getPartite($partite){
+    foreach ($partite as $partita) {
+        echo $partita['squadra_casa'] . ' - ' . $partita['squadra_ospite'] . ' | ' . $partita['punti_casa'] . '-' . $partita['punti_ospite'] . '<br>';
+    }
 }
-
-
-
+function getData(){
+    if (isset($_GET['name']) && isset($_GET['email']) && isset($_GET['age'])) {
+        $name = $_GET['name'];
+        $email = $_GET['email'];
+        $age = $_GET['age'];
+        if (empty($name) || empty($email) || empty($age)) {
+            echo 'Inserire dei valori nei campi';
+        } else {
+            control($name, $email, $age);
+        }
+    }
+}
+function control($name,$email,$age){
+    if (strlen($name) > 2 && strpos($email, '.') !== false && strpos($email, '@') && is_numeric($age)) {
+        //echo "nome: $name email: $email età: $age";
+        echo "Accesso riuscito";
+    } else {
+        echo "Accesso negato";
+    }    
+}
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP snack 1</title>
+    <title>PHP snack </title>
 </head>
+
 <body>
     <section>
         <h2>Snack 1</h2>
-    <?php 
-    foreach( $partite as $partita){
-        echo $partita['squadra_casa'].' - '.$partita['squadra_ospite'].' | '.$partita['punti_casa'].'-'.$partita['punti_ospite'].'<br>';
-    }
-    ?>
-    </section>
-    <h2>Snack 2</h2>
-    <form action="index.php" method="GET">
-    <input type="text" placeholder="Inserisci nome" name="name">
-    <input type="email" placeholder="Inserisci una email" name="email">
-    <input type="number" placeholder="Inserisci la tua età" name="age">
-    <button type="submit">Invia</button>
-    <?php
-        control();
+        <?php
+        getPartite($partite);
         ?>
-    </form>
-
+    </section>
     <section>
-
+        <h2>Snack 2</h2>
+        <form action="index.php" method="GET">
+            <input type="text" placeholder="Inserisci nome" name="name">
+            <input type="email" placeholder="Inserisci una email" name="email">
+            <input type="number" placeholder="Inserisci la tua età" name="age">
+            <button type="submit">Invia</button>
+        </form>
+            <?php
+           getData();
+            ?>
+    </section>
+    <section>
+        <h2>snak 3</h2>
     </section>
 </body>
+
 </html>
